fb287bf745a690600cb1b46382c559e098c2dab5
Ashish agarwal <aviinashagarwal@gmail.com>
2025-10-03 14:08:42 +0530

    logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -7,12 +8,12 @@ const Logo = () => {
     <>
     <div className='text-3xl w-full  text-center pt-5  h-fit fontJapanese'>
      
-<div className='  duration-300 ease-in-out  '>
+<Link href="/" className='  duration-300 ease-in-out  '>
 
 
 <span className='text-shadow-primary-foreground  '>Hiv</span>
 <span className='text-primary '>E</span>
-</div>
+</Link>
         
 
     </div>
